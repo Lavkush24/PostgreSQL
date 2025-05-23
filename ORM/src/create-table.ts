e2bb78cbcfa17019f3bda1f9058a63da09c72ffc
Lavkush <college.studying23@gmail.com>
2025-05-24 00:57:46 +0530

    complete setup for prisma ORM

---
+++ ORM/src/create-table.ts
@@ -0,0 +1,25 @@
+import { create } from 'domain'
+import { PrismaClient } from '../generated/prisma'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const user = await prisma.user.create({
+    data: {
+        email: "Lavkush@gmail.com",
+        name: "lavkush"
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
