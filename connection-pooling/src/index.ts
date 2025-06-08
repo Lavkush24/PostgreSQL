59d5c9a24c4fdf5fefd61347c08998a1d2766b39
Lavkush24 <college.studying23@gmail.com>
2025-06-08 12:10:33 +0530

    connection pooling: deployed succefuloy

---
+++ connection-pooling/src/index.ts
@@ -0,0 +1,25 @@
+
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export interface Env {
+  DATABASE_URL: string;
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    const prisma = new PrismaClient({
+      datasourceUrl: env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const users = await prisma.log.findMany();
+    const result = JSON.stringify(users);
+    return new Response(result);
+  },
+} satisfies ExportedHandler<Env>;
+
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
